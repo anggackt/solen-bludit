5c9036780f93fa495d31411b4856d1586564b9d2
Angga <146806205+anggackt@users.noreply.github.com>
2023-10-27 16:48:25 +0700

    Search button styles

--- php/home.php
+++ php/home.php
@@ -1,21 +1,21 @@
-                <?php foreach ($content as $pageTmp) : ?>
+                <?php foreach ($content as $page) : ?>
                 <article>
-                    <h2 class="judul_artikel"><a href="<?php echo $pageTmp->permalink() ?>"><?php echo $pageTmp->title() ?></a></h2>
+                    <h2 class="judul_artikel"><a href="<?php echo $page->permalink() ?>"><?php echo $page->title() ?></a></h2>
                     <div class="meta_top">
-                        <span class="meta_author"><img src="<?php echo Theme::src('img/user.svg'); ?>" alt="<?php echo $L->get('author') ?>" title="<?php echo $L->get('author') ?>" width="13" height="13">&nbsp;<?php echo $pageTmp->user('nickname'); ?></span>
-                        <span class="meta_read"><img src="<?php echo Theme::src('img/clock.svg'); ?>" alt="<?php echo $L->get('read') ?>" title="<?php echo $L->get('read') ?>" width="13" height="13">&nbsp;<?php echo $pageTmp->readingTime() ?></span>
-                        <time datetime="<?php echo $pageTmp->date('Y-m-d') ?>" class="meta_date"><img src="<?php echo Theme::src('img/calendar.svg'); ?>" alt="<?php echo $L->get('date') ?>" title="<?php echo $L->get('date') ?>" width="13" height="13">&nbsp;<?php echo $pageTmp->date() ?></time>
+                        <span class="meta_author"><img src="<?php echo Theme::src('img/user.svg'); ?>" alt="<?php echo $L->get('author') ?>" title="<?php echo $L->get('author') ?>" width="13" height="13">&nbsp;<?php echo $page->user('nickname'); ?></span>
+                        <span class="meta_read"><img src="<?php echo Theme::src('img/clock.svg'); ?>" alt="<?php echo $L->get('read') ?>" title="<?php echo $L->get('read') ?>" width="13" height="13">&nbsp;<?php echo $page->readingTime() ?></span>
+                        <time datetime="<?php echo $page->date('Y-m-d') ?>" class="meta_date"><img src="<?php echo Theme::src('img/calendar.svg'); ?>" alt="<?php echo $L->get('date') ?>" title="<?php echo $L->get('date') ?>" width="13" height="13">&nbsp;<?php echo $page->date() ?></time>
                     </div>
-                    <div class="featured_image card"><?php if ($pageTmp->coverImage()): ?><img src="<?php echo $pageTmp->coverImage(); ?>" alt="<?php echo $pageTmp->title(); ?>"><?php endif ?></div>
+                    <div class="featured_image card"><?php if ($page->coverImage()): ?><img src="<?php echo $page->coverImage(); ?>" alt="<?php echo $page->title(); ?>"><?php endif ?></div>
                     <div class="pratinjau_artikel">
-                        <?php if ($pageTmp->description()) : ?>
-                            <p><?php echo $pageTmp->description(); ?></p>
+                        <?php if ($page->description()) : ?>
+                            <p><?php echo $page->description(); ?></p>
                         <?php endif ?>
                     </div>
-                    <?php if (!empty($pageTmp->tags(true))): ?>
+                    <?php if (!empty($page->tags(true))): ?>
                     <div class="meta_bottom">
                         <ul class="meta_tag">
-                            <?php foreach ($pageTmp->tags(true) as $tagKey=>$tagName): ?>
+                            <?php foreach ($page->tags(true) as $tagKey=>$tagName): ?>
 								<li><a class="pseudo button" href="<?php echo DOMAIN_TAGS.$tagKey ?>"><?php echo $tagName; ?></a></li>
                             <?php endforeach ?>
                         </ul>
